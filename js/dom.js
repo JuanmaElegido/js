5af0db2c15fbbeac1c782d86a67d9735fcaff7b9
DESKTOP-ITAKOG4\juanm <juanmaelegido@gmail.com>
2022-02-13 12:36:25 +0100

    XMLHttpRequest

--- js/dom.js
+++ js/dom.js
@@ -3,3 +3,27 @@ console.log("hola mundo");
 var hola = "es un hola";
 
 console.log(window);
+
+let nombre = "Juanma";
+let apellido = "Casado";
+
+let saludo = `hola mi nombre es ${nombre} ${apellido}.`;
+
+console.log(saludo);
+let c = 7.19;
+
+console.log(c.toFixed(9));
+console.log(c.toFixed(1));
+
+let a = 2;
+
+console.log(++a);
+console.log(a++);
+
+const saludar = (nombre) => console.log(`Hola ${nombre}`);
+
+saludar("Juanma");
+
+const sumar = (a, b) => a + b;
+
+console.log(sumar(3, 4));
